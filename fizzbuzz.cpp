d20988d429869fd4503633a057cfd564735bba86
Robotics <moto.tsukisaka@gmail.com>
2014-05-13 06:57:27 +0900

    プレフィックス、｛｝の位置、インデント修正

--- fizzbuzz.cpp
+++ fizzbuzz.cpp
@@ -3,14 +3,13 @@
 void FizzBuzz(int nNumber)
 {
     char cFizzStr[] = "fizz";
-    char BuzzStr[] = "buzz";
+    char cBuzzStr[] = "buzz";
     
     if(nNumber%3 == 0) {
         printf("%s\n", cFizzStr);
     }
-    else if(nNumber%5 == 0)
-    {
-	    printf("%s\n",BuzzStr);
+    else if(nNumber%5 == 0) {
+        printf("%s\n",BuzzStr);
     }
     else {
         printf("%d\n", nNumber);
