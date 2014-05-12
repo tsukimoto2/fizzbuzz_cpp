fa7e90866041e39b177aa47f035daf5b2f98e446
Robotics <moto.tsukisaka@gmail.com>
2014-05-13 05:39:20 +0900

    5の倍数のときに"buzz"を表示

--- fizzbuzz.cpp
+++ fizzbuzz.cpp
@@ -3,9 +3,14 @@
 void FizzBuzz(int nNumber)
 {
     char cFizzStr[] = "fizz";
+    char BuzzStr[] = "buzz";
     
     if(nNumber%3 == 0) {
         printf("%s\n", cFizzStr);
+    }
+    else if(nNumber%5 == 0)
+    {
+	    printf("%s\n",BuzzStr);
     }
     else {
         printf("%d\n", nNumber);
